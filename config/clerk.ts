0b88d72ab54425d4baec93d59f86d5b5a95b92c1
Abinanthan47 <abinanthan247@gmail.com>
2025-06-12 22:39:42 +0530

    build working

---
+++ config/clerk.ts
@@ -0,0 +1,24 @@
+import { Clerk } from "@clerk/clerk-expo";
+import * as SecureStore from "expo-secure-store";
+
+const tokenCache = {
+  async getToken(key: string) {
+    try {
+      return SecureStore.getItemAsync(key);
+    } catch (err) {
+      return null;
+    }
+  },
+  async saveToken(key: string, value: string) {
+    try {
+      return SecureStore.setItemAsync(key, value);
+    } catch (err) {
+      return;
+    }
+  },
+};
+
+export const clerk = new Clerk({
+  tokenCache,
+  publishableKey: process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!,
+});
