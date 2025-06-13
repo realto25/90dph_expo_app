552f072b15a61596383495db41976e06330362c5
Priyadharshan <priyan2003chandru@gmail.com>
2025-06-13 09:06:58 +0530

    update

--- babel.config.js
+++ babel.config.js
@@ -6,7 +6,6 @@ module.exports = function (api) {
       'nativewind/babel',
     ],
     plugins: [
-      // REMOVED: 'expo-router/babel', // This line was removed
       'react-native-reanimated/plugin',
       [
         'module-resolver',
